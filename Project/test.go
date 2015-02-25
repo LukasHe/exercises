9648c65d5c4427e53e1d21d472a77ffb818f6b13
Patrik Levin <levinpatrik@gmail.com>
2015-02-25 18:00:18 +0100

    Changen on test

--- Project/test.go
+++ Project/test.go
@@ -1,15 +1,28 @@
 package main
 
 import "./driver"
-import "fmt"
+//import "fmt"
+//import "time"
 
 func main(){
 	ledOnChan := make(chan int, 10)
 	ledOffChan := make(chan int,10)
+	
+	motorDirChan := make(chan string, 10)
+	sensorChan := make(chan int, 10)
+	
 	keepAlive := make(chan int)
-	driver.DriverInit(ledOnChan, ledOffChan)
-	fmt.Println(LIGHT_DOWN3)
-	//ledOnChan <- LIGHT_DOWN3
+	
+	
+	driver.DriverInit(ledOnChan, ledOffChan, sensorChan, motorDirChan )
+	
+	ledOnChan <- driver.LIGHT_DOWN3
+	//motorDirChan <- "DOWN"
+	//time.Sleep(2000 * time.Millisecond)
+	//motorDirChan <- "UP"
+	//time.Sleep(2000 * time.Millisecond)
+	//motorDirChan <- "STOP"
+	
 	<- keepAlive
 	
 }
